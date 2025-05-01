45c4b9325cadaba10f356d1bc7cd09b401f9e1f0
Joao Henrique <joao.henrique1299@hotmail.com>
2025-05-01 18:11:13 -0300

    refactor: digest python steps separated by files

---
+++ digest/python/digest_mem_many_signatures.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#TODO: remove me
+rm -rf sign_message-many_times.csv verify_message-many_times.csv
+
+echo "creating files: sign_message-many_times.csv verify_message-many_times.csv"
+
+touch sign_message-many_times.csv
+touch verify_message-many_times.csv
+echo "input_in_messages_signed, memory_used_in_bytes" > sign_message-one_time.csv
+echo "input_in_messages_verified, memory_used_in_bytes" > verify_message-one_time.csv
+
+many_times_input_name="lorem-100k"
+many_message_file_list=$(ls | grep $many_times_input_name | sort -r)
+regex_get_n_messages_input='^bench-mem-([a-zA-Z0-9_]+)-([0-9]+)([kM])-([0-9]+)\.csv$'
+
+for file in ${many_message_file_list[@]}
+do
+  if [[ "$file" =~ $regex_get_n_messages_input ]]; then
+    axis_x=${BASH_REMATCH[4]}
+    axis_y_sign=$(cat $file | grep sign_message | awk '{print $2}')
+    axis_y_verify=$(cat $file | grep verify | awk '{print $2}')
+
+    echo "$axis_x , $axis_y_sign" >> sign_message-many_times.csv
+    echo "$axis_x , $axis_y_verify" >> verify_message-many_times.csv
+
+  else
+    echo "error retrieving amount of memory input from file; file=$file, regex=$regex_get_memory_input"
+  fi
+done
--- digest/python/main.sh
+++ digest/python/main.sh
@@ -10,74 +10,8 @@ if [[ $? -eq 0 ]]; then
   cp -r bench_mem digest/mem
   cd digest/mem
 
-  #TODO: remove me
-  rm -rf sign_message-one_time.csv verify_message-one_time.csv
-
-  echo "creating files: sign_message-one_time.csv verify_message-one_time.csv"
-
-  touch sign_message-one_time.csv
-  touch verify_message-one_time.csv
-  echo "input_in_bytes, memory_used_in_bytes" > sign_message-one_time.csv
-  echo "input_in_bytes, memory_used_in_bytes" > verify_message-one_time.csv
-
-  one_message_file_list=$(ls | grep "\-1\.csv")
-
-  regex_get_memory_input='^bench-mem-([a-zA-Z0-9_]+)-([0-9]+)([kM])-([0-9]+)\.csv$'
-
-  for file in ${one_message_file_list[@]}
-  do
-    if [[ "$file" =~ $regex_get_memory_input ]]; then
-      number="${BASH_REMATCH[2]}"
-      prefix="${BASH_REMATCH[3]}"
-
-      axis_x=0
-      if [[ $prefix == 'k' ]]; then
-        axis_x=$(($number * 1024))
-      elif [[ $prefix == 'M' ]]; then
-        axis_x=$(($number * 1024 * 1024))
-      else
-        echo "error unidentified prefix, valid prefixes are [kM]"
-      fi
-      axis_y_sign=$(cat $file | grep sign_message | awk '{print $2}')
-      axis_y_verify=$(cat $file | grep verify | awk '{print $2}')
-
-      echo "$axis_x , $axis_y_sign" >> sign_message-one_time.csv
-      echo "$axis_x , $axis_y_verify" >> verify_message-one_time.csv
-
-    else
-      echo "error retrieving amount of memory input from file; file=$file, regex=$regex_get_memory_input"
-    fi
-  done
-  
-  many_times_input_name="lorem-100k"
-  echo -n "creating files: sign_message-many_times.csv verify_message-many_times.csv;"
-  echo " message used to sign, verify : $many_times_input_name"
-
-  #TODO: remove me
-  rm -rf sign_message-many_times.csv verify_message-many_times.csv
-
-  touch sign_message-many_times.csv
-  touch verify_message-many_times.csv
-  echo "input_in_messages_signed, memory_used_in_bytes" > sign_message-one_time.csv
-  echo "input_in_messages_verified, memory_used_in_bytes" > verify_message-one_time.csv
-
-  many_message_file_list=$(ls | grep $many_times_input_name | sort -r)
-
-  for file in ${many_message_file_list[@]}
-  do
-    if [[ "$file" =~ $regex_get_memory_input ]]; then
-      axis_x=${BASH_REMATCH[4]}
-      axis_y_sign=$(cat $file | grep sign_message | awk '{print $2}')
-      axis_y_verify=$(cat $file | grep verify | awk '{print $2}')
-
-      echo "$axis_x , $axis_y_sign" >> sign_message-many_times.csv
-      echo "$axis_x , $axis_y_verify" >> verify_message-many_times.csv
-
-    else
-      echo "error retrieving amount of memory input from file; file=$file, regex=$regex_get_memory_input"
-    fi
-  done
-
+  $root/digest/python/digest_mem_one_signature.sh
+  $root/digest/python/digest_mem_many_signatures.sh
   # cleaning temp files
   ls | grep -Ev "^(sign|verify)" | xargs rm
 
