e537b8f7dc13726510a63372bdbac59c3fdad66b
Joao Henrique <joao.henrique1299@hotmail.com>
2025-05-01 18:55:02 -0300

    feat: digest python, prof files to csv

--- digest/python/main.sh
+++ digest/python/main.sh
@@ -8,21 +8,24 @@ mkdir digest
 if [[ $? -eq 0 ]]; then
   # digest mem
   cp -r bench_mem digest/mem
+
   cd digest/mem
+    $root/digest/python/digest_mem_one_signature.sh
+    $root/digest/python/digest_mem_many_signatures.sh
 
-  $root/digest/python/digest_mem_one_signature.sh
-  $root/digest/python/digest_mem_many_signatures.sh
-  # cleaning temp files
-  ls | grep -Ev "^(sign|verify)" | xargs rm
-
+    ## cleaning temp files
+    ls | grep -Ev "^(sign|verify)" | xargs rm
   cd ../../
+
   # digest cpu
-  # ls
-  # cp -r bench_cpu digest/cpu
-  # cd digest/cpu
-  # for digest_file in $(ls)
-  # do
-  #   echo "$digest_file"
-  # done
-  # cd ../../
+  cp -r bench_cpu digest/cpu
+  cd digest/cpu
+    echo "transforming .prof files to .csv"
+    for file_prof in $(ls)
+    do
+      python3 $root/digest/python/transform_prof_to_csv.py $file_prof
+      rm $file_prof
+    done
+  
+  cd ../../
 fi
--- prof.py
+++
@@ -1,7 +0,0 @@
-import pstats
-
-p = pstats.Stats("profile.prof")
-p.strip_dirs().sort_stats('cumulative')
-
-# Print statistics only for functions matching 'sign_message' or 'verify_message'
-p.print_stats(r'sign_message_entrypoint|verify_message_entrypoint')
