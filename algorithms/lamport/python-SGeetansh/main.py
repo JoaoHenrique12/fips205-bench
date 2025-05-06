252670a5e3e60cab4c4bdb2d18c655916622abfc
Joao Henrique <joao.henrique1299@hotmail.com>
2025-05-06 20:22:49 -0300

    feat: initial version of lamport python-SGeetansh implementation

---
+++ algorithms/lamport/python-SGeetansh/main.py
@@ -0,0 +1,59 @@
+import os
+from typing import Any
+#import tracemalloc #HACK: bench_mem
+from ls import keygen, signgen, verification
+
+def sign_message_entrypoint(message: str) -> Any:
+    keypair = keygen()
+    signature = signgen(message, keypair[0])
+    return signature, keygen
+
+def verify_message_entrypoint(message: str, signature: Any, keypair: Any) -> bool:
+    is_valid = verification(message, keypair[1], signature)
+    return is_valid
+
+
+def main():
+    n_messages_str = os.environ.get("N_MESSAGES", "1")
+    message_path = os.environ.get("MESSAGE_PATH")
+
+    if message_path is None:
+        raise ValueError("MESSAGE_PATH env var not defined !")
+
+    try:
+        n_messages = int(n_messages_str)
+    except ValueError:
+        raise ValueError("N_MESSAGES must be an integer if defined!")
+
+#    tracemalloc.start() #HACK: bench_mem
+
+    with open(message_path, "r") as m:
+        message = ''.join(m.readlines())
+
+#    total_memory_sign = 0 #HACK: bench_mem
+#    total_memory_verify = 0 #HACK: bench_mem
+
+    for _ in range(n_messages):
+#        snapshot_before_sign = tracemalloc.take_snapshot() #HACK: bench_mem
+        signature, keygen = sign_message_entrypoint(message)
+#        snapshot_after_sign = tracemalloc.take_snapshot() #HACK: bench_mem
+
+#        stats_sign = snapshot_after_sign.compare_to(snapshot_before_sign, 'filename') #HACK: bench_mem
+#        memory_diff_sign = sum(stat.size_diff for stat in stats_sign) #HACK: bench_mem
+#        total_memory_sign += memory_diff_sign #HACK: bench_mem
+
+#        snapshot_before_verify = tracemalloc.take_snapshot() #HACK: bench_mem
+        verify_message_entrypoint(message, signature, keygen)
+#        snapshot_after_verify = tracemalloc.take_snapshot() #HACK: bench_mem
+
+#        stats_verify = snapshot_after_verify.compare_to(snapshot_before_verify, 'filename') #HACK: bench_mem
+#        memory_diff_verify = sum(stat.size_diff for stat in stats_verify) #HACK: bench_mem
+#        total_memory_verify += memory_diff_verify #HACK: bench_mem
+
+#    input_name = os.path.basename(message_path).split('.')[0] #HACK: bench_mem
+#    with open(f"bench-mem-{input_name}-{n_messages}.csv", "w") as f: #HACK: bench_mem
+#        f.write(f"sign_message, {total_memory_sign}\n") #HACK: bench_mem
+#        f.write(f"verify_message, {total_memory_verify}\n") #HACK: bench_mem
+
+if __name__ == "__main__":
+    main()
