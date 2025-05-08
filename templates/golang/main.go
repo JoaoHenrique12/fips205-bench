6a0b78656e784f16c405da8b2218ef616e6c5249
Joao Henrique <joao.henrique1299@hotmail.com>
2025-05-07 21:12:58 -0300

    feat(template): golang select if wish cpu or mem profiling

--- templates/golang/main.go
+++ templates/golang/main.go
@@ -22,6 +22,7 @@ func verifyMessageEntrypoint(message string, signature string) bool {
 }
 
 func main() {
+	// Reading env vars
 	nMessagesStr := os.Getenv("N_MESSAGES")
 	if nMessagesStr == "" {
 		nMessagesStr = "1"
@@ -46,26 +47,54 @@ func main() {
 	}
 	message := string(messageBytes)
 
+	profilingType := os.Getenv("PROFILING_TYPE")
+	if profilingType != "mem" && profilingType != "cpu" {
+		fmt.Println("PROFILING_TYPE env var validation error options available: ! [mem|cpu]")
+		os.Exit(1)
+	}
+
+	// mounting output name
+	inputName := filepath.Base(messagePath)
+	inputName = inputName[:len(inputName)-len(filepath.Ext(inputName))] // remove the sufix
+	fileName := fmt.Sprintf("bench-%s-%s-%d.prof", profilingType, inputName, nMessages)
+
+  if profilingType == "cpu" {
+  	// Create CPU profile file
+  	cpuFile, err := os.Create(fileName)
+  	if err != nil {
+	  	fmt.Fprintf(os.Stderr, "could not create CPU profile: %v\n", err)
+	  	os.Exit(1)
+  	}
+  	defer cpuFile.Close()
+
+  	// Start CPU profiling
+  	if err := pprof.StartCPUProfile(cpuFile); err != nil {
+	  	fmt.Fprintf(os.Stderr, "could not start CPU profile: %v\n", err)
+	  	os.Exit(1)
+  	}
+  	defer pprof.StopCPUProfile()
+  }
+
+
 	for i := 0; i < nMessages; i++ {
 		signature := signMessageEntrypoint(message)
 		verifyMessageEntrypoint(message, signature)
 		_ = signature
 	}
 
-	inputName := filepath.Base(messagePath)
-	inputName = inputName[:len(inputName)-len(filepath.Ext(inputName))]
-	fileName := fmt.Sprintf("bench-mem-%s-%d.prof", inputName, nMessages)
+	if profilingType == "mem" {
 
-	f, err := os.Create(fileName)
-	if err != nil {
-		fmt.Println("could not create memory profile: ", err)
-		os.Exit(1)
-	}
-	defer f.Close()
+		f, err := os.Create(fileName)
+		if err != nil {
+			fmt.Println("could not create memory profile: ", err)
+			os.Exit(1)
+		}
+		defer f.Close()
 
-	runtime.GC()
-	if err := pprof.WriteHeapProfile(f); err != nil {
-		fmt.Println("could not write memory profile: ", err)
-		os.Exit(1)
+		runtime.GC()
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			fmt.Println("could not write memory profile: ", err)
+			os.Exit(1)
+		}
 	}
 }
