148af4733a80683231a24844ac193514419c4088
Joao Henrique <joao.henrique1299@hotmail.com>
2025-05-10 20:15:09 -0300

    refactor(template): golang main.go creating only profiling files

--- templates/golang/main.go
+++ templates/golang/main.go
@@ -4,25 +4,71 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"runtime/pprof"
 	"runtime"
+	"runtime/pprof"
 	"strconv"
 )
 
 func signMessageEntrypoint(message string) string {
 	signature := ""
-  // use the imported library here
+	// use the imported library here
 	return signature
 }
 
 func verifyMessageEntrypoint(message string, signature string) bool {
 	isValid := true
-  // use the imported library here
+	// use the imported library here
 	return isValid
 }
 
 func main() {
-	// Reading env vars
+	messagePath, nMessages, profilingType := readEnvVars()
+	profilingFileName := getProfilingOutputName(messagePath, nMessages, profilingType)
+
+	messageBytes, err := os.ReadFile(messagePath)
+	if err != nil {
+		fmt.Printf("Error reading message file: %v\n", err)
+		os.Exit(1)
+	}
+	message := string(messageBytes)
+
+	if profilingType == "cpu" {
+		cpuFile, err := os.Create(profilingFileName)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "could not create CPU profile: %v\n", err)
+			os.Exit(1)
+		}
+		defer cpuFile.Close()
+
+		if err := pprof.StartCPUProfile(cpuFile); err != nil {
+			fmt.Fprintf(os.Stderr, "could not start CPU profile: %v\n", err)
+			os.Exit(1)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
+	for i := 0; i < nMessages; i++ {
+		signature := signMessageEntrypoint(message)
+		verifyMessageEntrypoint(message, signature)
+	}
+
+	if profilingType == "mem" {
+		memFile, err := os.Create(profilingFileName)
+		if err != nil {
+			fmt.Println("could not create memory profile: ", err)
+			os.Exit(1)
+		}
+		defer memFile.Close()
+
+		runtime.GC()
+		if err := pprof.WriteHeapProfile(memFile); err != nil {
+			fmt.Println("could not write memory profile: ", err)
+			os.Exit(1)
+		}
+	}
+}
+
+func readEnvVars() (string, int, string) {
 	nMessagesStr := os.Getenv("N_MESSAGES")
 	if nMessagesStr == "" {
 		nMessagesStr = "1"
@@ -40,61 +86,19 @@ func main() {
 		os.Exit(1)
 	}
 
-	messageBytes, err := os.ReadFile(messagePath)
-	if err != nil {
-		fmt.Printf("Error reading message file: %v\n", err)
-		os.Exit(1)
-	}
-	message := string(messageBytes)
-
 	profilingType := os.Getenv("PROFILING_TYPE")
 	if profilingType != "mem" && profilingType != "cpu" {
 		fmt.Println("PROFILING_TYPE env var validation error options available: ! [mem|cpu]")
 		os.Exit(1)
 	}
 
-	// mounting output name
+	return messagePath, nMessages, profilingType
+}
+
+func getProfilingOutputName(messagePath string, nMessages int, profilingType string) string {
 	inputName := filepath.Base(messagePath)
 	inputName = inputName[:len(inputName)-len(filepath.Ext(inputName))] // remove the sufix
 	fileName := fmt.Sprintf("bench-%s-%s-%d.prof", profilingType, inputName, nMessages)
 
-  if profilingType == "cpu" {
-  	// Create CPU profile file
-  	cpuFile, err := os.Create(fileName)
-  	if err != nil {
-	  	fmt.Fprintf(os.Stderr, "could not create CPU profile: %v\n", err)
-	  	os.Exit(1)
-  	}
-  	defer cpuFile.Close()
-
-  	// Start CPU profiling
-  	if err := pprof.StartCPUProfile(cpuFile); err != nil {
-	  	fmt.Fprintf(os.Stderr, "could not start CPU profile: %v\n", err)
-	  	os.Exit(1)
-  	}
-  	defer pprof.StopCPUProfile()
-  }
-
-
-	for i := 0; i < nMessages; i++ {
-		signature := signMessageEntrypoint(message)
-		verifyMessageEntrypoint(message, signature)
-		_ = signature
-	}
-
-	if profilingType == "mem" {
-
-		f, err := os.Create(fileName)
-		if err != nil {
-			fmt.Println("could not create memory profile: ", err)
-			os.Exit(1)
-		}
-		defer f.Close()
-
-		runtime.GC()
-		if err := pprof.WriteHeapProfile(f); err != nil {
-			fmt.Println("could not write memory profile: ", err)
-			os.Exit(1)
-		}
-	}
+	return fileName
 }
